34618e137fa1439a3d03c81ed492a2827a2a4c3c
idealogica <fre6lime@yandex.ru>
2020-11-11 00:31:13 +0300

    CommandLineFormatter implemented

---
+++ src/Formatter/CommandLineFormatter.php
@@ -0,0 +1,13 @@
+<?php
+namespace Idealogica\ErrorHandler\Formatter;
+
+use League\BooBoo\Formatter\CommandLineFormatter as BooBooCommandLineFormatter;
+
+/**
+ * Class CommandLineFormatter
+ * @package Idealogica\ErrorHandler\Formatter
+ */
+class CommandLineFormatter extends BooBooCommandLineFormatter
+{
+    use FormatterTrait;
+}
